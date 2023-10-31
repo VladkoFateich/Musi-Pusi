77c0bf3d13535e12040bed1426893003c02db7d2
Vladislav Fateev <Fateev_wbd@mail.ru>
2023-10-31 09:45:14 +0500

    slider

--- slider.js
+++ slider.js
@@ -1,18 +1,22 @@
-    let swiper = new Swiper(".mySwiper", {
-      loop: true,
-      spaceBetween: 10,
-      slidesPerView: 16,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-    let swiper2 = new Swiper(".mySwiper2", {
-      loop: true,
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      thumbs: {
-        swiper: swiper,
-      },
-    });
+let swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 16,
+  breakpoints: {
+    340: { slidesPerView: 5 },
+    740: {slidesPerView: 9,},
+  },
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
